8f0880fbbb5e6b2c22d3e724d6aba266e6efa361
Diego Feitoza <diego.feitoza@accenture.com>
2021-01-26 17:22:11 -0300

    Alura Imersão ReactJs e NextJS - V2

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget'
+
+// Montando componente a partir do styled component
+const Background = styled.div`
+    background-image: url(${db.bg});
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+// Montando componente a partir de uma função JS
+// function Title(props){ //Propcidade/propriedades
+//   return (
+//     <h1>
+//       {props.children}
+//     </h1>
+//   )
+// }
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Background>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Pokemon</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Hello World! Hello World! Hello World! Hello World! </p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quiz da Galera</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>Hello World! Hello World! Hello World! Hello World! </p>
+          </Widget.Content>
+
+        </Widget>
+      </QuizContainer>
+    </Background>
+  );
 }
