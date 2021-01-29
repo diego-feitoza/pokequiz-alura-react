19bc3c338ad783fe7d85286b0f68b43466057db6
Diego Feitoza <diegofeitoza.dev@gmail.com>
2021-01-29 00:55:54 -0300

    Aula 3

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,13 @@ import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 // Montando componente a partir do styled component
 // const Background = styled.div`
@@ -17,16 +20,6 @@ import GitHubCorner from '../src/components/GitHubCorner';
 //     background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {  
   const router = useRouter();
@@ -51,16 +44,20 @@ export default function Home() {
               //Router para a proxima pagina
             }}
             >
-              <input placeholder="Fala teu nome ai =]" 
-              onChange={(e) => {
-                // name = e.target.value
-                setName(e.target.value)
-              }}
+              <Input
+                name="nomeUsuario"
+                placeholder="Fala teu nome ai =]" 
+                onChange={(e) => {
+                  // name = e.target.value
+                  setName(e.target.value)
+                }}
               />
-              <button type="submit" disabled={!name}>
+              <Button
+                type="submit"
+                disabled={!name}>
                 Jogar
-                {/* {name} */}
-              </button>
+                {name ? ` | ${name}` : ``}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,48 +1,163 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
 import React from 'react';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 
 import db from '../db.json';
 import Widget from '../src/components/Widget';
+import QuizContainer from '../src/components/QuizContainer';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
-import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Button from '../src/components/Button';
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
+const LoadingWidget = () => {
+  return(
+    <Widget>
+      <Widget.Header>
+        Pokequiz
+      </Widget.Header>
+      <Widget.Content>
+        <p>
+          Loading...
+        </p>
+      </Widget.Content>
+    </Widget>
+  )
+}
 
-export default function QuizPage() {
-    const routes = useRouter();   
-    const name = routes.query.name;
+const ResultQuiz = () => {
+  return(
+    <Widget>
+      <Widget.Header>
+        Pokequiz
+      </Widget.Header>
+      <Widget.Content>
+        <p>
+          Parabéns! Você acertou X questões
+        </p>
+      </Widget.Content>
+    </Widget>
+  )
+}
+
+
+const QuesionWidget = ({
+  question, 
+  totalQuestion,
+  questionId,
+  onSubmit
+}) => {
+  const questionName = `querstionName__${questionId}`;
+  return(
+    <Widget>
+        <Widget.Header>
+          {/* <BackLinkArrow href="/" /> */}
+      <h2>
+        {`Pergunta ${questionId+1} de ${totalQuestion}`}
+      </h2>
+        </Widget.Header>
+        <img
+          alt="Descrição"
+          style={{
+            width: '100%',
+            height: '150px',
+            objectFit: 'cover',
+          }}
+          src={question.image}
+        />
+        <Widget.Content>
+          <h2>
+              {question.title}
+          </h2>
+          <p>
+            {question.description}
+          </p>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              onSubmit();
+            }}
+            >
+            {question.alternatives.map((alternative, alternativeIndex) => {
+              // console.log(alternative)
+              const alternativeId = `alternative__${alternativeIndex}`
+              return (
+                <Widget.Topic
+                  as="label"
+                  htmlFor={alternativeId}
+                >
+                  <input 
+                    // style={{display:'none'}}
+                    id={alternativeId}
+                    name={questionName}
+                    type="radio"
+                    />   
+                  {alternative}                   
+                </Widget.Topic>        
+              );
+            })} 
+            <Button type="submit">
+              Confirmar
+            </Button>  
+          </form>  
+        </Widget.Content>
+      </Widget>
+  )
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT'
+}
+
+// [Ciclo de vida do ReactJS || React chama de Efeitos : Effects]
+// useEffect() - é o nascer do componente
+// assim que nasce === didMount
+// quando for ser atualizado === willUpdate
+// quando vai morrer === willUnmount
+
+export default function QuizPage() {    
+    const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+    // console.log('Perguntas criadas: ', db.questions);
+    const [currentQuestion, setCurrentQuestion] = React.useState(0);
+    const questionId = currentQuestion;
+    const question = db.questions[questionId];
+    const totalQuestion = db.questions.length;
+  
+    // const name = routes.query.name; //Nome user do query
+  
+    React.useEffect(() => {
+      //fetch() ...
+      setTimeout(() => {
+        setScreenState(screenStates.QUIZ);
+      },1 * 1000)
+      //nasce === didMount
+    },[]) //callback);
+
+    function handleSubmitQuiz() {
+      const nextQuestion = questionId + 1;
+      if (nextQuestion < totalQuestion) {
+        setCurrentQuestion(nextQuestion);
+      } else {
+        setScreenState(screenStates.RESULT);
+      }
+    }
+
     return(
         <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
-          <Widget.Header>
-            <h1>
-                Olá =] {name}                 
-            </h1>
-          </Widget.Header>
-          <Widget.Content>
-            <h2>
-                Vamos jogar?
-            </h2>
-            <p>
-                Mussum Ipsum, cacilds vidis litro abertis. Detraxit consequat et quo num tendi nada. Cevadis im ampola pa arma uma pindureta. Pra lá , depois divoltis porris, paradis. Manduma pindureta quium dia nois paga.
-            </p>
-          </Widget.Content>
-        </Widget>
-        <Footer />
+        {screenState === screenStates.QUIZ && (
+          <QuesionWidget
+            question={question}
+            totalQuestion={totalQuestion}
+            questionId={questionId}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+        {screenState === screenStates.RESULT && <ResultQuiz />}
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/diego-feitoza/pokequiz-alura-react" />
     </QuizBackground>
